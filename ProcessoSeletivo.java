19b3a02808a71390d8caabcab5b516d3812d6f16
Pedro Henrique <pedrohenriquecamargo2019@gmail.com>
2024-05-06 18:41:50 -0300

    primeira parte concluida

--- ProcessoSeletivo.java
+++ ProcessoSeletivo.java
@@ -1,8 +1,21 @@
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
+
+        Analisador(2500.0);
+        Analisador(1500.0);
+        Analisador(2000.0);
     }
 
+    static void Analisador(double salarioPretendido) {
+        double salarioBase = 2000.0;
 
-
+        if (salarioBase > salarioPretendido) {
+            System.out.println("\nLIGAR PARA CANDIDATO.");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("\nLIGAR E FAZER CONTRA PROPOSTA.");
+        } else {
+            System.out.println("\nAGUARDAR OUTROS CANDIDATOS.");
+        }
+    }
 }
